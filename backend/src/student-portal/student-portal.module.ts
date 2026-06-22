3a8e096b63005191db03f389c58bad5c3c79a898
sudarshanshinde1012-sudo <sudarshanshinde1012@gmail.com>
2026-06-22 16:54:35 +0530

    Fix MaintenanceModeGuard dependency injection

--- backend/src/student-portal/student-portal.module.ts
+++ backend/src/student-portal/student-portal.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentPortalController } from './student-portal.controller';
 import { QuizzesModule } from '../quizzes/quizzes.module';
+import { SettingsModule } from '../settings/settings.module';
 
 @Module({
-  imports: [QuizzesModule],
+  imports: [QuizzesModule, SettingsModule],
   controllers: [StudentPortalController],
 })
 export class StudentPortalModule {}
--- backend/src/students/students.module.ts
+++ backend/src/students/students.module.ts
@@ -6,9 +6,10 @@ import { Student } from '../entities/student.entity';
 import { QuizResult } from '../entities/quiz-result.entity';
 import { QuizAttempt } from '../entities/quiz-attempt.entity';
 import { Quiz } from '../entities/quiz.entity';
+import { SettingsModule } from '../settings/settings.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student, QuizResult, QuizAttempt, Quiz])],
+  imports: [TypeOrmModule.forFeature([Student, QuizResult, QuizAttempt, Quiz]), SettingsModule],
   providers: [StudentsService],
   controllers: [StudentsController],
   exports: [StudentsService],
