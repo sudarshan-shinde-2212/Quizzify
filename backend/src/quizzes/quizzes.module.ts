ed8b7fa52827c81656df9ad52c3217e87372a7e4
sudarshanshinde1012-sudo <sudarshanshinde1012@gmail.com>
2026-06-22 16:12:13 +0530

    Fix QuizzesModule repository injection

--- backend/src/quizzes/quizzes.module.ts
+++ backend/src/quizzes/quizzes.module.ts
@@ -3,9 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuizzesService } from './quizzes.service';
 import { QuizzesController } from './quizzes.controller';
 import { Quiz } from '../entities/quiz.entity';
+import { QuizResult } from '../entities/quiz-result.entity';
+import { Student } from '../entities/student.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Quiz])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Quiz,
+      QuizResult,
+      Student,
+    ]),
+  ],
   providers: [QuizzesService],
   controllers: [QuizzesController],
   exports: [QuizzesService],
